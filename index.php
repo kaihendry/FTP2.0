a558ae75ad869491c56cc4e618369c9ff76b42c5
Kai Hendry <hendry@webconverger.com>
2013-10-10 15:11:23 +0800

    Support multiple file uploads Thanks https://twitter.com/cheeaun/status/387879608693301248

--- index.php
+++ index.php
@@ -6,32 +6,24 @@
 <style>
 body { margin: 3em; font-size: 1.6em; font-family: sans-serif; text-align: center; background-color: white; }
 input { font-size: 1.1em; }
-form { border: 2.6em green solid; padding: 1em; background-color: green; -moz-border-radius: 1em; -webkit-border-radius: 1em; border-radius: 1em; }
+form { border: 2.6em #42d451 solid; padding: 1em; background-color: #42d451; -moz-border-radius: 1em; -webkit-border-radius: 1em; border-radius: 1em; }
 </style>
 </head>
 <body>
 <?php
 include("auth.php");
-if (getAdmin()) { echo '<p>Found cookie!</p>'; } else { die('<h1>You do not have the secret cookie to allow you to upload!</h1>
+if (! getAdmin()) { die('<h1>You do not have the secret cookie to allow you to upload!</h1>
 	<p><a href=/foo.php>Get one temporarily!</a></p>'); }
 ?>
 
-<?php echo "<h1>Hello ☺</h1>";  ?>
-
 <form action="/upload.php" method="post" enctype="multipart/form-data">
-<input type="file" name="f" accept="image/*" capture="camera">
+<input name="f[]" type="file" multiple="" />
 <input value="Upload" type="submit">
 </form>
 
-<p><a href="https://github.com/kaihendry/FTP2.0">Source code</a></p>
-
-<pre>
-~$ grep -E 'filesize|max_size' /etc/php5/apache2/php.ini
-post_max_size = 50M
-upload_max_filesize = 50M
-</pre>
-
 <pre>curl -f -H 'Cookie: ftptwo=<?php echo $_COOKIE['ftptwo']; ?>' -F "f=@/tmp/foobar.png" http://<?php echo $_SERVER["HTTP_HOST"]; ?>/upload.php</pre>
+
+<p><a href="https://github.com/kaihendry/FTP2.0">MIT licensed source code</a></p>
 
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -5,14 +5,16 @@ if (! getAdmin()) {
 	die("Unauthorized");
 }
 
+// header("Content-Type: text/plain");
+// print_r($_FILES);
+// echo "Here: " . $_FILES['f']['tmp_name'][0] . "\n";
+// echo "Here: " . $_FILES['f']['tmp_name'] . "\n";
+// echo "Here: " . count($_FILES["f"]["name"]) . "\n";
+
+// die("wtf");
+
+
 date_default_timezone_set('Asia/Singapore');
-
-//header("Content-Type: text/plain");
-//print_r($_FILES);
-
-if (! is_uploaded_file($_FILES['f']['tmp_name'])) {
-	die("Upload fail: Missing file");
-}
 
 $dir = getcwd();
 if (is_writable($dir)) {
@@ -20,22 +22,40 @@ if (is_writable($dir)) {
 } else {
 	die("No write permission.\n Fix with: chown -R www-data $dir");
 }
-
 @mkdir($dir, 0777);
 
-$name = pathinfo($_FILES['f']['name'], PATHINFO_FILENAME);
-$extension = pathinfo($_FILES['f']['name'], PATHINFO_EXTENSION);
+if (! is_array($_FILES["f"]["name"])) {
 
-$increment = ''; //start with no suffix
+	if (! is_uploaded_file($_FILES['f']['tmp_name'])) { die("Upload fail: Missing file " . $_FILES["f"]["name"]); }
 
-while(file_exists("$dir/" . $name . $increment . '.' . $extension)) {
-	$increment++;
+	$name = pathinfo($_FILES['f']['name'], PATHINFO_FILENAME);
+	$extension = pathinfo($_FILES['f']['name'], PATHINFO_EXTENSION);
+	$increment = ''; //start with no suffix
+
+	while(file_exists("$dir/" . $name . $increment . '.' . $extension)) { $increment++; }
+
+	$incname = "$dir/" . $name . $increment . '.' . $extension;
+	move_uploaded_file($_FILES["f"]['tmp_name'], $incname);
+
+} else {
+
+	for ($i = 0; $i < count($_FILES["f"]["name"]); $i++) {
+
+	if (! is_uploaded_file($_FILES['f']['tmp_name'][$i])) { die("Upload fail: Missing file " . $_FILES["f"]["name"][$i]); }
+
+	$name = pathinfo($_FILES['f']['name'][$i], PATHINFO_FILENAME);
+	$extension = pathinfo($_FILES['f']['name'][$i], PATHINFO_EXTENSION);
+	$increment = ''; //start with no suffix
+
+	while(file_exists("$dir/" . $name . $increment . '.' . $extension)) { $increment++; }
+
+	$incname = "$dir/" . $name . $increment . '.' . $extension;
+	move_uploaded_file($_FILES["f"]['tmp_name'][$i], $incname);
+
+	}
 }
 
-$basename = "$dir/" . $name . $increment . '.' . $extension;
-
-move_uploaded_file($_FILES["f"]['tmp_name'], $basename);
-
+@rmdir($dir); // remove directory if empty
 header("Location: http://" . $_SERVER["HTTP_HOST"] . '/' . basename($dir));
 
 ?>
